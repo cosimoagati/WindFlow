0b7201c9cfbdbad6f93d84d3c401d37b6c9199c5
cosimone64 <cosimo.agati@gmail.com>
2020-02-02 12:20:49 +0100

    Add dir-locals file for Emacs.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((c++-mode . ((flycheck-clang-include-path . ("/usr/local/include/"
+					      "/usr/local/include/wf"))
+	      (flycheck-gcc-include-path . ("/usr/local/include/"
+					    "/usr/local/include/wf")))))
