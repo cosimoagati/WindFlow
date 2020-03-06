6dc27b038685ee45acf639667e780dd82e4f70fb
cosimone64 <cosimo.agati@disroot.org>
2020-03-06 03:09:45 +0100

    ...

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((c++-mode . ((flycheck-clang-include-path . ("~workspaces/windflow-scratchpad"
 					      "~workspaces/windflow-scratchpad/wf"))
 	      (flycheck-gcc-include-path . ("~workspaces/windflow-scratchpad"
-					    "~workspaces/windflow-scratchpad/wf"))
-	      (flycheck-clang-language-standard . "c++17")
-	      (flycheck-gcc-language-standard . "c++17"))))
+					    "~workspaces/windflow-scratchpad/wf")))
+	   (flycheck-clang-language-standard . "c++17")
+	   (flycheck-gcc-language-standard . "c++17")))
