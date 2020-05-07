616c53ffba3cb00f8edce7a0994b095befcd3660
cosimone64 <cosimo.agati@disroot.org>
2020-05-07 18:27:12 +0200

    Cosmetics.

--- src/individual_test_gpu/cuda_invocable_example.cu
+++ src/individual_test_gpu/cuda_invocable_example.cu
@@ -15,28 +15,26 @@ struct is_invocable :
 auto f = [] __device__ (int &x) { x = x * x; };
 
 template<typename func_t>
-int
-g(func_t f, int x)
-{
+int (func_t f, int x) {
 	cout << is_invocable<func_t, int&>::value << endl;
 	cout << is_invocable<func_t, int&, string>::value << endl;
 	f(x);
 }
 
 template<typename tuple_t, typename result_t, typename func_t>
-class Example
-{
+class Example {
 	func_t f;
 public:
-	Example (func_t f) : f {f}
+	Example (func_t f)
+	: f {f}
+
 	{
 		cout << is_invocable<func_t, int&>::value << endl;
 		cout << is_invocable<func_t, int&, string&>::value << endl;
 	}
 };
 
-int main()
-{
+int main() {
 	cout << is_invocable<decltype(f), int&>::value << endl;
 	cout << is_invocable<decltype(f), string>::value << endl;
 	Example<int, int, decltype(f)> e {f};
