92212a7724cf1c7fe9d7c4dda9a3d25b8e51129b
cosimone64 <cosimo.agati@disroot.org>
2020-05-21 16:19:56 +0200

    Change function names and #ifdefs.

--- src/individual_test_gpu/map_gpu_test.cu
+++ src/individual_test_gpu/map_gpu_test.cu
@@ -30,6 +30,7 @@ struct tuple_t {
 	tuple_t(size_t key, uint64_t id, uint64_t ts, int64_t value)
 		: key {key}, id {id}, ts {ts}, value{value}
 	{}
+
 	tuple_t() : key {0}, id {0}, ts {0}, value {0} {}
 
 	tuple<size_t, uint64_t, uint64_t> getControlFields() const {
@@ -46,7 +47,7 @@ template<typename tuple_t>
 class Source : public ff_node_t<tuple_t, tuple_t> {
 	static constexpr auto LIMIT = 1000;
 	long counter {0};
-#ifndef PERFORMANCE_TEST
+#ifdef PERFORMANCE_TEST
 	time_point<steady_clock> start_time {steady_clock::now()};
 	time_point<steady_clock> end_time = start_time + seconds {10};
 #endif
@@ -57,12 +58,12 @@ public:
 	}
 	tuple_t *svc(tuple_t *) {
 #ifdef PERFORMANCE_TEST
-		if (counter > LIMIT) {
+		const auto current_time = steady_clock::now();
+		if (current_time >= end_time) {
 			return this->EOS;
 		}
 #else
-		const auto current_time = steady_clock::now();
-		if (current_time >= end_time) {
+		if (counter > LIMIT) {
 			return this->EOS;
 		}
 #endif
@@ -94,7 +95,8 @@ void closing_func(RuntimeContext &) {}
 int routing_func(const size_t k, const size_t n) { return k % n; }
 
 void test_gpu() {
-	const auto mult_and_div = [] __host__ __device__ (tuple_t &x) {
+#ifdef PERFORMANCE_TEST
+	const auto ip_keyless_func = [] __host__ __device__ (tuple_t &x) {
 		// Deliberately long-to-compute function.  Cheap functions that
 		// cost less than buffering are not representative.
 		auto divide = false;
@@ -107,12 +109,19 @@ void test_gpu() {
 			divide = !divide;
 		}
 	};
-	const auto another_square = [] __host__ __device__ (const tuple_t &x, tuple_t &y) {
+#else
+	const auto ip_keyless_func = [] __host__ __device__ (tuple_t &x) {
+		x.value *= x.value;
+	};
+#endif
+#ifndef PERFORMANCE_TEST
+	const auto nip_keyless_func = [] __host__ __device__ (const tuple_t &x,
+							      tuple_t &y) {
 		y.value = x.value * x.value;
 	};
-	const auto verify_order = [] __host__ __device__ (tuple_t &t,
-							  char *const scratchpad,
-							  const std::size_t size) {
+	const auto ip_keyed_func = [] __host__ __device__ (tuple_t &t,
+							   char *const scratchpad,
+							   const std::size_t size) {
 		// NB: the first tuple must have value 0!
 		assert(size >= sizeof(int));
 		assert(scratchpad != nullptr);
@@ -123,10 +132,10 @@ void test_gpu() {
 		}
 		*reinterpret_cast<int *>(scratchpad) = t.value;
 	};
-	const auto verify_order_nip = [] __host__ __device__ (const tuple_t &t,
-							      tuple_t &r,
-							      char *const scratchpad,
-							      const std::size_t size) {
+	const auto nip_keyed_func = [] __host__ __device__ (const tuple_t &t,
+							    tuple_t &r,
+							    char *const scratchpad,
+							    const std::size_t size) {
 		// NB: the first tuple must have value 0!
 		assert(size >= sizeof(int));
 		assert(scratchpad != nullptr);
@@ -136,9 +145,10 @@ void test_gpu() {
 		}
 		*reinterpret_cast<int *>(scratchpad) = r.value;
 	};
+#endif
 	ff_pipeline ip_pipe;
 	ip_pipe.add_stage(::Source<tuple_t> {});
-	auto ip_map = MapGPU_Builder<decltype(mult_and_div)> {mult_and_div}
+	auto ip_map = MapGPU_Builder<decltype(ip_keyless_func)> {ip_keyless_func}
 		.withName("gpu_ip_map")
 		.withParallelism(1)
 		.build_ptr();
@@ -152,11 +162,12 @@ void test_gpu() {
 	delete ip_map; // Sadly needed since copy elision for builders only
 		       // works from C++17 onwards.
 
+#ifndef PERFORMANCE_TEST
 	output_stream << "In-pace pipeline finished, now testing non in-place version..."
 		      << endl;
 	ff_pipeline nip_pipe;
 	nip_pipe.add_stage(::Source<tuple_t> {});
-	auto nip_map = MapGPU_Builder<decltype(another_square)> {another_square}
+	auto nip_map = MapGPU_Builder<decltype(nip_keyless_func)> {nip_keyless_func}
 		.withName("gpu_nip_map")
 		.withParallelism(1)
 		.build_ptr();
@@ -175,10 +186,10 @@ void test_gpu() {
 	ip_keyed_pipe.add_stage(::Source<tuple_t> {});
 
 	// TODO: Builder still needs a correct get_tuple_t meta_utils function!
-	// auto ip_keyed_map = MapGPU_Builder<decltype(verify_order)> {verify_order}.withParallelism(1)
+	// auto ip_keyed_map = MapGPU_Builder<decltype(ip_keyed_func)> {ip_keyed_func}.withParallelism(1)
 	// 										  .enable_KeyBy()
 	// 										  .build_ptr();
-	MapGPU<tuple_t, tuple_t, decltype(verify_order)> ip_keyed_map {verify_order,
+	MapGPU<tuple_t, tuple_t, decltype(ip_keyed_func)> ip_keyed_map {ip_keyed_func,
 			1, "gpu_ip_keyed_map", routing_func, 256, 256, sizeof(int)};
 	ip_keyed_pipe.add_stage(&ip_keyed_map);
 	ip_keyed_pipe.add_stage(::Sink<tuple_t> {});
@@ -186,12 +197,14 @@ void test_gpu() {
 		error("Error while running pipeline");
 		std::exit(EXIT_FAILURE);
 	}
+#endif
 }
 
+#ifdef PERFORMANCE_TEST
 void test_cpu() {
 	output_stream << "Testing \"default\", CPU Map..." << endl;
 
-	const auto mult_and_div = [] (tuple_t &x) {
+	const auto ip_keyless_func = [] (tuple_t &x) {
 		// Deliberately long-to-compute function.  Cheap functions that
 		// cost less than buffering are not representative.
 		auto divide = false;
@@ -204,10 +217,10 @@ void test_cpu() {
 			divide = !divide;
 		}
 	};
-	const auto another_square = [] (const tuple_t &x, tuple_t &y) {
+	const auto nip_keyless_func = [] (const tuple_t &x, tuple_t &y) {
 		y.value = x.value * x.value;
 	};
-	const auto verify_order = [] (tuple_t &t, char *const scratchpad,
+	const auto ip_keyed_func = [] (tuple_t &t, char *const scratchpad,
 				      const std::size_t size) {
 		// NB: the first tuple must have value 0!
 		assert(size >= sizeof(int));
@@ -219,10 +232,10 @@ void test_cpu() {
 		}
 		*reinterpret_cast<int *>(scratchpad) = t.value;
 	};
-	const auto verify_order_nip = [] (const tuple_t &t, tuple_t &r, char *scratchpad,
+	const auto ip_keyed_func_nip = [] (const tuple_t &t, tuple_t &r, char *scratchpad,
 					  std::size_t size) {
 		// NB: the first tuple must have value 0!
-		assert(size >= sizeof(int)
+		assert(size >= sizeof(int));
 		assert(scratchpad != nullptr);
 		const auto prev_val = static_cast<int>(*scratchpad);
 		if (t.value == -1 || (t.value != 0 && t.value <= prev_val)) {
@@ -232,7 +245,7 @@ void test_cpu() {
 	};
 	ff_pipeline ip_pipe;
 	ip_pipe.add_stage(::Source<tuple_t> {});
-	auto ip_map = Map_Builder<decltype(mult_and_div)> {mult_and_div}
+	auto ip_map = Map_Builder<decltype(ip_keyless_func)> {ip_keyless_func}
 		.withName("cpu_ip_map")
 		.withParallelism(1)
 		.build_ptr();
@@ -246,6 +259,7 @@ void test_cpu() {
 	delete ip_map; // Sadly needed since copy elision for builders only
 		       // works from C++17 onwards.
 }
+#endif
 
 int main(int argc, char *argv[]) {
 	test_gpu();
