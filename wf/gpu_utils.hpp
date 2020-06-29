c44f4db8a744d471219e19e1090ed7d90e9c719d
cosimone64 <cosimo.agati@disroot.org>
2020-06-29 02:17:38 +0200

    Add GPUBufferHandle destructor.
    Needed when deleting GPUBufferHandle objects, otherwise the
    pointed CUDA buffers leak.

--- wf/gpu_utils.hpp
+++ wf/gpu_utils.hpp
@@ -61,6 +61,8 @@ template<typename T>
 struct GPUBufferHandle {
 	T *buffer;
 	int size; // Because making std::size_t unsigned was a mistake.
+
+	~GPUBufferHandle() { cudaFree(buffer); }
 };
 
 inline void failwith(const std::string &err) {
