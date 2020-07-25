1a74249408f93b54858a6da32b602dc7105a9c42
cosimone64 <cosimo.agati@disroot.org>
2020-07-25 05:03:02 +0200

    Update large scan.

--- src/individual_test_gpu/cuda_scan_example.cu
+++ src/individual_test_gpu/cuda_scan_example.cu
@@ -1,11 +1,83 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include "../../wf/gpu_utils.hpp"
 
 using namespace std;
 
-__global__ void prescan(int *const g_odata, int *const g_idata, const int n,
-			const int target_value, const int power_of_two) {
+__global__ void prescan_large(int *const output, int *const input,
+			      int *const sums, const int end,
+			      const int target_value,
+			      const int power_of_two) {
+	extern __shared__ int mapped_idata[];
+	const auto absolute_thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+	const auto stride = blockDim.x * gridDim.x;
+	const auto block_thread_id = threadIdx.x;
+
+	for (auto i = block_thread_id; i < power_of_two; i += stride) {
+		mapped_idata[i] = input[i] == target_value;
+	}
+	// TODO: zero-out the rest of the array?
+	// if (absolute_thread_id / power_of_two < 1) {
+	// 	for (auto i = block_thread_id; i < power_of_two; i += stride) {
+	// 		mapped_idata[i] = 0;
+	// 	}
+	// }
+	// TODO: bit shifts should be more efficient...
+	for (auto stride = 1; stride <= power_of_two; stride *= 2) {
+		__syncthreads();
+		const auto index = (block_thread_id + 1) * stride * 2 - 1;
+		if (index < 2 * power_of_two) {
+			mapped_idata[index] += mapped_idata[index - stride];
+		}
+	}
+	for (auto stride = power_of_two / 2; stride > 0; stride /= 2) {
+		__syncthreads();
+		const auto index = (block_thread_id + 1) * stride * 2 - 1;
+		if ((index + stride) < 2 * power_of_two) {
+			mapped_idata[index + stride] += mapped_idata[index];
+		}
+	}
+	__syncthreads();
+	if (block_thread_id == 0) {
+		sums[blockIdx.x] = mapped_idata[end - 1];
+	}
+	__syncthreads();
+	// Perform a parallel prefix sum on the partial sum array.
+	if (blockIdx.x == 0) {
+		for (auto stride = 1; stride <= power_of_two; stride *= 2) {
+			__syncthreads();
+			const auto index = (block_thread_id + 1) * stride * 2 - 1;
+			if (index < 2 * power_of_two) {
+				sums[index] += sums[index - stride];
+			}
+		}
+		for (auto stride = power_of_two / 2; stride > 0; stride /= 2) {
+			__syncthreads();
+			const auto index = (block_thread_id + 1) * stride * 2 - 1;
+			if ((index + stride) < 2 * power_of_two) {
+				sums[index + stride] += sums[index];
+			}
+		}
+		// Sum partial values to the sum of the preceding block.
+		if (blockIdx.x > 0) {
+			for (auto i = block_thread_id; i < power_of_two; i += stride) {
+				mapped_idata[i] += sums[blockIdx.x - 1];
+			}
+		}
+	}
+	// Simple, but probably inefficient and redundant: write data to output.
+	__syncthreads();
+	for (auto i = absolute_thread_id; i < end; i += stride) {
+		output[i] = mapped_idata[i];
+	}
+}
+
+__global__ void unmapped_scan(int *const )
+
+
+__global__ void prescan(int *const g_odata, int *const g_idata,
+			const int n, const int target_value, const int pow) {
 	extern __shared__ int mapped_idata[];
 	const auto index = blockIdx.x * blockDim.x + threadIdx.x;
 	const auto stride = blockDim.x * gridDim.x;
@@ -15,22 +87,22 @@ __global__ void prescan(int *const g_odata, int *const g_idata, const int n,
 	for (auto i = index; i < n; i += stride) {
 		mapped_idata[i] = g_idata[i] == target_value;
 	}
-	// TODO: zero-out the rest of the array?
-	for (auto i = n; i < power_of_two; i += stride) {
-		mapped_idata[i] = 0;
-	}
+	// TODO: zero-out the rest of the array? Is this necessary?
+	// for (auto i = n; i < pow; i += stride) {
+	// 	mapped_idata[i] = 0;
+	// }
 	// TODO: bit shifts should be more efficient...
-	for (auto stride = 1; stride <= power_of_two; stride *= 2) {
+	for (auto stride = 1; stride <= pow; stride *= 2) {
 		__syncthreads();
 		const auto index = (thread_id + 1) * stride * 2 - 1;
-		if (index < 2 * power_of_two) {
+		if (index < 2 * pow) {
 			mapped_idata[index] += mapped_idata[index - stride];
 		}
 	}
-	for (auto stride = power_of_two / 2; stride > 0; stride /= 2) {
+	for (auto stride = pow / 2; stride > 0; stride /= 2) {
 		__syncthreads();
 		const auto index = (thread_id + 1) * stride * 2 - 1;
-		if ((index + stride) < 2 * power_of_two) {
+		if ((index + stride) < 2 * pow) {
 			mapped_idata[index + stride] += mapped_idata[index];
 		}
 	}
@@ -38,6 +110,29 @@ __global__ void prescan(int *const g_odata, int *const g_idata, const int n,
 	__syncthreads();
 	for (auto i = index; i < n; i += stride) {
 		g_odata[i] = mapped_idata[i];
+	}
+}
+
+void mapped_scan(int *const output, int *const input, const int size,
+		 const int target_value) {
+	static constexpr auto threads_per_block = 512;
+	auto pow = 1;
+	if (size <= threads_per_block) {
+		while (pow < size) { // Get closest power of two above or equal to size.
+			pow *= 2;
+		}
+		prescan<<<1, pow, 2 * pow * sizeof(int)>>>(output, input, size, target_value, pow);
+	} else {
+		const auto num_of_blocks = size / threads_per_block + 1;
+		while (pow < threads_per_block) {
+			pow *= 2;
+		}
+		GPUBuffer<int> partial_sums {num_of_blocks - 1};
+		prescan_large<<<num_of_blocks, 2 * pow * sizeof(int)>>>
+			(output, input, partial_sums.data(), size, target_value, pow);
+		int cpu_partial_sums[num_of_blocks - 1];
+		cudaMemcpy(cpu_partial_sums, partial_sums.data(), partial_sums.size() * sizeof(int). cudaMemcpyDeviceToHost);
+		cout << "a" << endl;
 	}
 }
 
@@ -56,14 +151,6 @@ __global__ void create_sub_batch(tuple_t *const bin,
 	}
 }
 
-int get_closest_power_of_two(const int n) {
-	auto i = 1;
-	while (i < n) {
-		i *= 2;
-	}
-	return i;
-}
-
 int main(const int argc, char *const argv[]) {
 	if (argc <= 1) {
 		cerr << "Use as " << argv[0] << " <space-separated elements>\n";
@@ -73,26 +160,19 @@ int main(const int argc, char *const argv[]) {
 	const auto n = argc - 1;
 	vector<int> cpu_index;
 	vector<int> cpu_scan;
-	int *gpu_index;
-	int *gpu_scan;
-
 	cpu_scan.resize(n);
 	for (auto i = 1; i < argc; ++i) {
 		cpu_index.push_back(std::atoi(argv[i]));
 	}
-	if (cudaMalloc(&gpu_scan, n * sizeof *gpu_scan) != cudaSuccess) {
-		cerr << "Error allocating gpu_scan.\n";
-		return -1;
-	}
-	if (cudaMalloc(&gpu_index, n * sizeof *gpu_index) != cudaSuccess) {
-		cerr << "Error allocating gpu_index.\n";
-		return -1;
-	}
-	cudaMemcpy(gpu_index, cpu_index.data(), n * sizeof *gpu_index, cudaMemcpyHostToDevice);
-	const auto pow = get_closest_power_of_two(n);
-	prescan<<<1, pow, 2 * pow * sizeof *gpu_scan>>>(gpu_scan, gpu_index, n, target_value, pow);
+	GPUBuffer<int> gpu_scan {n};
+	GPUBuffer<int> gpu_index {n};
+
+	cudaMemcpy(gpu_index.data(), cpu_index.data(),
+		   n * sizeof *gpu_index.data(), cudaMemcpyHostToDevice);
+	mapped_scan(gpu_scan.data(), gpu_index.data(), n, target_value);
 	cudaDeviceSynchronize();
-	cudaMemcpy(cpu_scan.data(), gpu_scan, n * sizeof *gpu_scan, cudaMemcpyDeviceToHost);
+	cudaMemcpy(cpu_scan.data(), gpu_scan.data(),
+		   n * sizeof *gpu_scan.data(), cudaMemcpyDeviceToHost);
 	cout << "Size is " << n << '\n';
 	cout << "Result of scan: ";
 	for (const auto &x : cpu_scan) {
@@ -100,11 +180,6 @@ int main(const int argc, char *const argv[]) {
 	}
 	cout << "\n";
 	std::size_t bout_size = cpu_scan[n - 1];
-	const auto bout_raw_size = bout_size * sizeof(int);
-	int *bout;
-	if (cudaMalloc(&bout, bout_raw_size) != cudaSuccess) {
-		cerr << "Failed to allocate partial output batch.";
-		return -1;
-	}
+	GPUBuffer<std::size_t> bout {bout_size};
 	return 0;
 }
