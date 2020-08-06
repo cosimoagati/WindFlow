e7c0d0774fbd5fd2ce68040d881f4a4c76aa3642
cosimone64 <cosimo.agati@disroot.org>
2020-08-06 15:38:36 +0200

    Increase line length.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,5 @@
 	      (flycheck-gcc-include-path . ("~workspaces/windflow-scratchpad"
 					    "~workspaces/windflow-scratchpad/wf"))
               (flycheck-clang-language-standard . "c++17")
-              (flycheck-gcc-language-standard . "c++17"))))
+              (flycheck-gcc-language-standard . "c++17")
+	      (fill-column . 110))))
