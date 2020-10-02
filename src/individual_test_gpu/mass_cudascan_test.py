cc93636f1378b57540b77e9c7748dfeb68d29ca4
cosimone64 <cosimo.agati@disroot.org>
2020-10-02 02:21:10 +0200

    Add arguments.

--- src/individual_test_gpu/mass_cudascan_test.py
+++ src/individual_test_gpu/mass_cudascan_test.py
@@ -2,9 +2,15 @@
 
 import random
 import subprocess
+import sys
+
+if (len(sys.argv) == 1):
+    number_of_runs = 64
+else:
+    number_of_runs = int(sys.argv[1])
 
 arglist = []
-for i in range(64):
+for i in range(number_of_runs):
     arglist.append(str(random.randint(0, 1)))
     print(arglist, flush=True)
     subprocess.run(["./a.out"] + arglist)
