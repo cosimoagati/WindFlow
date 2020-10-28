9518ede3b533cb929d6778fd209b26372c3be31e
cosimone64 <cosimo.agati@disroot.org>
2020-10-28 12:24:46 +0100

    Improve test script.

--- GPU_Tests/stateful/run_tests.py
+++ GPU_Tests/stateful/run_tests.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import subprocess
 
-with open("stateful_results.txt", "w") as output_file:
+FILE = "stateful-results-improved.txt"
+
+with open(FILE, "w") as output_file:
     for testnum in range(1, 7):
         for keynum in [1, 10, 100, 500, 960, 1000, 2000, 4000, 8000, 10000]:
-            arglist = ["./test_stateful_v" + str(testnum), "-l 50000000",
-                       "-b 10000", "-n 1", "-k " + str(keynum)]
+            arglist = ["./test_stateful_v" + str(testnum), "-l", "50000000",
+                       "-b", "10000", "-n", "1", "-k", str(keynum)]
             print(arglist)
-            subprocess.run(arglist, stdout=output_file)
+            output_file.write(str(arglist) + "\n")
+            output_file.flush()
+            subprocess.run(arglist, stdout=output_file, check=True)
+
+with open(FILE, "r+") as output_file:
+    lines = output_file.readlines()
+    output_file.seek(0)
+    for line in lines:
+        if not line.startswith("[SINK]") or "total" in line:
+            output_file.write(line)
+    output_file.truncate()
