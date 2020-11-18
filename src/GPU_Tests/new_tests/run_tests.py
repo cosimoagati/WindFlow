dc5a11b8004698c0842b4c121404ee7b4066e9a9
cosimone64 <cosimo.agati@disroot.org>
2020-11-18 23:47:25 +0100

    Cosmetics.

--- src/GPU_Tests/new_tests/run_tests.py
+++ src/GPU_Tests/new_tests/run_tests.py
@@ -9,9 +9,8 @@ with open(FILE, 'w') as output_file:
     for test in ['gpu_map_stateless', 'gpu_map_stateful',
                  'gpu_filter_stateless', 'gpu_filter_stateful']:
         for keynum in [1, 10, 100, 500, 960, 1000, 2000, 4000, 8000, 10000]:
-            arglist = ['./' + test, '-s', '1',
-                       '-b', '10000', '-n', '1', '-k', str(keynum),
-                       '-f', 'sensors.dat']
+            arglist = ['./' + test, '-s', '1', '-b', '10000', '-n', '1', '-k',
+                       str(keynum), '-f', 'sensors.dat']
             print(arglist)
             output_file.write(str(arglist) + '\n')
             output_file.flush()
