415df7068504579d42c13edb5b655a204826e2c5
cosimone64 <cosimo.agati@disroot.org>
2020-12-03 21:47:30 +0100

    Clean up and add parameters to test scripts.

--- src/GPU_Tests/new_tests/run_tests.py
+++ src/GPU_Tests/new_tests/run_tests.py
@@ -5,39 +5,48 @@ import sys
 FILE = 'testresults.txt'
 STATELESS_TESTS = ['gpu_map_stateless', 'gpu_filter_stateless']
 STATEFUL_TESTS = ['gpu_map_stateful', 'gpu_filter_stateful']
+KEY_AMOUNTS = [1, 10, 100, 500, 960, 1000, 2000, 4000, 8000, 10000]
+SOURCES_NUM = 1
+STREAM_LENGTH = 50000000
+BATCH_LENGTH = 10000
+WORKER_NUM = 1
+DATASET_FILE = 'sensors.dat'
 
 
-def error():
+def print_usage_and_exit():
+    """Print out correct usage in case of wrong invocation."""
     sys.stderr.write('Use as ' + sys.argv[0] + ' all|stateless|stateful\n')
     sys.exit(-1)
 
 
-if len(sys.argv) != 2:
-    error()
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
 
-choice = sys.argv[1].lower()
-if choice == 'all':
-    tests = STATELESS_TESTS + STATEFUL_TESTS
-elif choice == 'stateless':
-    tests = STATELESS_TESTS
-elif choice == 'stateful':
-    tests = STATEFUL_TESTS
-else:
-    error()
+    choice = sys.argv[1].lower()
+    if choice == 'all':
+        tests = STATELESS_TESTS + STATEFUL_TESTS
+    elif choice == 'stateless':
+        tests = STATELESS_TESTS
+    elif choice == 'stateful':
+        tests = STATEFUL_TESTS
+    else:
+        print_usage_and_exit()
 
-print('Starting tests, writing results to ' + FILE)
-with open(FILE, 'w') as output_file:
-    for test in tests:
-        for keynum in [1, 10, 100, 500, 960, 1000, 2000, 4000, 8000, 10000]:
-            arglist = ['./' + test, '-s', '1', '-b', '10000', '-n', '1', '-k',
-                       str(keynum), '-f', 'sensors.dat']
-            print(arglist)
-            output_file.write(str(arglist) + '\n')
+    print('Starting tests, writing results to ' + FILE)
+    with open(FILE, 'w') as output_file:
+        for test in tests:
+            for keynum in KEY_AMOUNTS:
+                arglist = ['./' + test, '-s', str(SOURCES_NUM), '-b',
+                           str(BATCH_LENGTH), '-n', str(WORKER_NUM), '-k',
+                           str(keynum), '-f', DATASET_FILE]
+                print(arglist)
+                output_file.write(str(arglist) + '\n')
+                output_file.flush()
+
+                subprocess.run(arglist, stdout=output_file, check=True)
+                output_file.write('\n')
+                output_file.flush()
+
+            output_file.write('\n\n')
             output_file.flush()
-
-            subprocess.run(arglist, stdout=output_file, check=True)
-            output_file.write('\n')
-            output_file.flush()
-
-        output_file.write('\n\n')
-        output_file.flush()
--- src/GPU_Tests/stateful/run_tests.py
+++ src/GPU_Tests/stateful/run_tests.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 import subprocess
 
-FILE = "stateful-results-zipf.txt"
+FILE = "stateful-results-uniform.txt"
+KEY_AMOUNTS = [1, 10, 100, 500, 960, 1000, 2000, 4000, 8000, 10000]
+STREAM_LENGTH = 50000000
+BATCH_LENGTH = 10000
+WORKER_NUM = 1
 
-print('Starting tests, writing results to ' + FILE)
+if __name__ == '__main__':
+    print('Starting tests, writing results to ' + FILE)
+    with open(FILE, 'w') as output_file:
+        for testnum in range(1, 7):
+            for keynum in KEY_AMOUNTS:
+                arglist = ['./test_stateful_v' + str(testnum), '-l',
+                           str(STREAM_LENGTH), '-b', str(BATCH_LENGTH), '-n',
+                           str(WORKER_NUM), '-k', str(keynum)]
+                print(arglist)
+                output_file.write(str(arglist) + '\n')
+                output_file.flush()
 
-with open(FILE, "w") as output_file:
-    for testnum in range(1, 7):
-        for keynum in [1, 10, 100, 500, 960, 1000, 2000, 4000, 8000, 10000]:
-            arglist = ["./test_stateful_v" + str(testnum), "-l", "50000000",
-                       "-b", "10000", "-n", "1", "-k", str(keynum)]
-            print(arglist)
-            output_file.write(str(arglist) + "\n")
+                subprocess.run(arglist, stdout=output_file, check=True)
+                output_file.write('\n')
+                output_file.flush()
+
+            output_file.write('\n\n')
             output_file.flush()
 
-            subprocess.run(arglist, stdout=output_file, check=True)
-            output_file.write("\n")
-            output_file.flush()
-
-        output_file.write("\n\n")
-        output_file.flush()
-
-with open(FILE, "r+") as output_file:
-    lines = output_file.readlines()
-    output_file.seek(0)
-    for line in lines:
-        if not line.startswith("[SINK]") or "total" in line:
-            output_file.write(line)
-    output_file.truncate()
+    with open(FILE, 'r+') as output_file:
+        lines = output_file.readlines()
+        output_file.seek(0)
+        for line in lines:
+            if not line.startswith('[SINK]') or 'total' in line:
+                output_file.write(line)
+        output_file.truncate()
