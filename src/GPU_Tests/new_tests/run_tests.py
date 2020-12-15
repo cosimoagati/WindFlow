d4af0f67dff0ccd080deb532eb8d859b819185c4
cosimone64 <cosimo.agati@disroot.org>
2020-12-15 23:13:09 +0100

    Add new tests to test script.

--- src/GPU_Tests/new_tests/run_tests.py
+++ src/GPU_Tests/new_tests/run_tests.py
@@ -5,6 +5,10 @@ import sys
 FILE = 'testresults.txt'
 STATELESS_TESTS = ['gpu_map_stateless', 'gpu_filter_stateless']
 STATEFUL_TESTS = ['gpu_map_stateful', 'gpu_filter_stateful']
+EXTRA_MAP_TESTS = ['gpu_map_stateful_one_per_warp',
+                   'gpu_map_stateful_no_warps']
+EXTRA_FILTER_TESTS = ['gpu_filter_stateful_one_per_warp',
+                      'gpu_filter_stateful_no_warps']
 KEY_AMOUNTS = [1, 10, 100, 500, 960, 1000, 2000, 4000, 8000, 10000]
 SOURCES_NUM = 1
 STREAM_LENGTH = 50000000
@@ -15,7 +19,9 @@ DATASET_FILE = 'sensors.dat'
 
 def print_usage_and_exit():
     """Print out correct usage in case of wrong invocation."""
-    sys.stderr.write('Use as ' + sys.argv[0] + ' all|stateless|stateful\n')
+    sys.stderr.write('Use as ' +
+                     sys.argv[0] +
+                     ' all|stateless|stateful|mapextra|filterextra\n')
     sys.exit(-1)
 
 
@@ -25,11 +31,16 @@ if __name__ == '__main__':
 
     choice = sys.argv[1].lower()
     if choice == 'all':
-        tests = STATELESS_TESTS + STATEFUL_TESTS
+        tests = (STATELESS_TESTS + STATEFUL_TESTS + EXTRA_MAP_TESTS +
+                 EXTRA_FILTER_TESTS)
     elif choice == 'stateless':
         tests = STATELESS_TESTS
     elif choice == 'stateful':
         tests = STATEFUL_TESTS
+    elif choice == 'mapextra':
+        tests = EXTRA_MAP_TESTS
+    elif choice == 'filterextra':
+        tests = EXTRA_FILTER_TESTS
     else:
         print_usage_and_exit()
 
